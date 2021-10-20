c6afcf31f199070b5f8711657b8a6c1904879dcf
Vitali Pisani <88674055+Vitali115@users.noreply.github.com>
2021-10-20 15:32:20 +0200

    Aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
+// Dati inseriti dall' utente.
 const firstname = prompt("Inserisci il tuo nome");
 const lastname = prompt("Inserisci il tuo cognome");
 const favColor = prompt("Inserisci il tuo colore preferito");
-
 console.log(firstname);
 console.log(lastname);
 console.log(favColor);
-
+// 
 const randonNumb= Math.floor(Math.random() *115);
 console.log(randonNumb);
-
+// 
 let firstMessage = "La tua password è pronta!"
 console.log(firstMessage);
-
-
-welcome = `<h2>Ciao ${firstname}<br>${firstMessage}</h2>`;
+// 
+let welcome = `<h2>Ciao ${firstname}<br>${firstMessage}</h2>`;
 document.getElementById('welcome').innerHTML = welcome;
-
-
+// 
 const pass = firstname + lastname + favColor + randonNumb;
 console.log(pass);
 document.getElementById('pass').innerHTML = pass;
-
-
+// 
+passleng = `<h2>${pass.length} caratteri</h2> `;
+console.log(passleng);
+document.getElementById('passleng').innerHTML = passleng;
 
 
